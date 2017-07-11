60bae27325054c4b9f96bf29a139e39fa5d2a306
Bart Oldeman <bart.oldeman@calculquebec.ca>
2017-07-11 12:03:32 +0000

    Unhid nixpkgs/.16.09 to nixpkgs/16.09; add imkl in default StdEnv

--- nixpkgs/16.09.lua
+++ nixpkgs/16.09.lua
@@ -1,8 +1,12 @@
+help([[Nixpkgs is a collection of packages for the Nix package manager - Homepage: https://github.com/NixOS/nixpkgs]])
+whatis("Description: Nixpkgs is a collection of packages for the Nix package manager - Homepage: https://github.com/NixOS/nixpkgs")
+
 add_property(   "lmod", "sticky")
 
 local root = "/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09"
 
 setenv("NIXUSER_PROFILE", root)
+prepend_path("PATH", "/cvmfs/soft.computecanada.ca/custom/bin")
 prepend_path("PATH", pathJoin(root, "sbin"))
 prepend_path("PATH", pathJoin(root, "bin"))
 prepend_path("LIBRARY_PATH", pathJoin(root, "lib"))
@@ -20,6 +24,8 @@ setenv("CURL_CA_BUNDLE", "/etc/pki/tls/certs/ca-bundle.crt")
 setenv("PAGER", "less -R")
 setenv("LESSOPEN", "|" .. pathJoin(root, "bin/lesspipe.sh %s"))
 setenv("LOCALE_ARCHIVE", pathJoin(root, "lib/locale/locale-archive"))
+-- silence harmless MXM warnings from libmxm
+setenv("MXM_LOG_LEVEL", "error")
 -- workaround for id issue
 prepend_path("LD_LIBRARY_PATH", "/usr/lib64/nvidia")
 prepend_path("LD_LIBRARY_PATH", "/cvmfs/soft.computecanada.ca/nix/lib")
@@ -30,6 +36,9 @@ prepend_path("PATH", "/cvmfs/soft.computecanada.ca/easybuild/bin")
 prepend_path("PYTHONPATH", "/cvmfs/soft.computecanada.ca/easybuild/lib/python2.7/site-packages")
 setenv("EASYBUILD_CONFIGFILES", "/cvmfs/soft.computecanada.ca/easybuild/config.cfg")
 setenv("EASYBUILD_BUILDPATH", pathJoin("/dev/shm", os.getenv("USER")))
+
+setenv("EBROOTNIXPKGS", root)
+setenv("EBVERSIONNIXPKGS", "16.09")
 
 prepend_path("MODULEPATH", "/cvmfs/soft.computecanada.ca/easybuild/modules/2017/Core")
 if os.getenv("USER") ~= "ebuser" then
