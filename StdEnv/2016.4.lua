a1acb83c03b5cfe7d39d7f04caac0145dd682ad6
Bart Oldeman <bart.oldeman@calculquebec.ca>
2017-07-11 13:44:59 +0000

    Remove /opt directories from StdEnv, as there are no more duplicates.

--- StdEnv/2016.4.lua
+++ StdEnv/2016.4.lua
@@ -5,10 +5,6 @@ local root = "/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09"
 require("os")
 --if os.getenv("LMOD_SYSTEM_NAME") == "cedar" then
 	load("nixpkgs/16.09")
-	-- ensure that these paths are ahead of Nix's. 
-	prepend_path("PATH","/opt/software/slurm/bin")
-	prepend_path("PATH","/opt/puppetlabs/puppet/bin")
-	prepend_path("PATH","/opt/software/bin")
 	load("intel")
 	load("imkl")
 	load("openmpi")
