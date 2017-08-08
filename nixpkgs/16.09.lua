0f5d637f44e98a30a53a6126729b0d305c64bd0f
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2017-08-08 13:48:14 +0000

    added RSNT_ARCH and RSNT_INTERCONNECT in nixpkgs

--- nixpkgs/16.09.lua
+++ nixpkgs/16.09.lua
@@ -31,8 +31,25 @@ setenv("MXM_LOG_LEVEL", "error")
 -- workaround for id issue
 prepend_path("LD_LIBRARY_PATH", "/cvmfs/soft.computecanada.ca/nix/lib")
 
--- also make easybuild and easybuild-generated modules accessible
 require("os")
+-- Define RSNT variables
+-- do something only at load time, if not already defined. Otherwise, unloading the module will lose the predefined value if there is one
+local cc_cluster = os.getenv("CC_CLUSTER") or "computecanada"
+if not os.getenv("RSNT_ARCH") and mode() == "load" then
+	if cc_cluster == "cedar" or cc_cluster == "graham" or cc_cluster == "computecanada" then
+		setenv("RSNT_ARCH", "avx2")
+	end
+end
+if not os.getenv("RSNT_INTERCONNECT") and mode() == "load" then
+	if cc_cluster == "cedar" then
+		setenv("RSNT_INTERCONNECT", "omnipath")
+	else
+		setenv("RSNT_INTERCONNECT", "infiniband")
+	end
+end
+
+
+-- also make easybuild and easybuild-generated modules accessible
 prepend_path("PATH", "/cvmfs/soft.computecanada.ca/easybuild/bin")
 setenv("EASYBUILD_CONFIGFILES", "/cvmfs/soft.computecanada.ca/easybuild/config.cfg")
 setenv("EASYBUILD_BUILDPATH", pathJoin("/dev/shm", os.getenv("USER")))
