12a27c03738d01f83bdd1be9d407e70257710500
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2018-05-03 20:47:49 +0000

    added singularity 2.5 module

---
+++ singularity/2.5.lua
@@ -0,0 +1,17 @@
+help([[Singularity is an application containerization solution for High-Performance Computing (HPC). The goal
+of Singularity is to allow for "mobility of computing": an application containerized on one Linux system should 
+be able to run on another system, as it is, and without the need to reconcile software dependencies and Linux 
+version differences between the source and target systems. 
+- Website: http://singularity.lbl.gov/ 
+- CC-Wiki: Singularity ]])
+whatis([[Description: Singularity is an application containerization solution for High-Performance Computing (HPC). The goal
+of Singularity is to allow for "mobility of computing": an application containerized on one Linux system should 
+be able to run on another system, as it is, and without the need to reconcile software dependencies and Linux 
+version differences between the source and target systems. 
+- Website: http://singularity.lbl.gov/ 
+- CC-Wiki: Singularity ]])
+
+local root = "/opt/software/singularity-2.5"
+
+prepend_path("PATH", pathJoin(root, "bin"))
+
