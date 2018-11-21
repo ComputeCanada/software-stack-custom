5d662ea38bd8aac2da29dc7a01ed48885b4ea219
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2018-11-21 15:42:22 +0000

    modified StdEnv/2016.4 and added StdEnv/2018.3

--- StdEnv/2018.3.lua
+++ StdEnv/2018.3.lua
@@ -4,5 +4,5 @@ local root = "/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09"
 
 require("os")
 load("nixpkgs/16.09")
-load("intel/2016.4")
-load("openmpi/2.1.1")
+load("intel/2018.3")
+load("openmpi/3.1.2")
