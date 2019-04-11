901ca9c211791de24a3af6fbcc3729091057c387
Bart Oldeman <bart.oldeman@calculquebec.ca>
2019-04-11 13:47:28 +0000

    Handle MODULERCFILE and default StdEnv versions via StdEnv module.
    Also let StdEnv load the default openmpi; this allows switching
    compilers that have different openmpi versions.

--- modules/StdEnv/.modulerc.lua
+++ modules/StdEnv/.modulerc.lua
@@ -1,1 +1,5 @@
-module_version("StdEnv/2016.4", "default")
+if os.getenv("CC_CLUSTER") == "beluga" or os.getenv("CC_CLUSTER") == "niagara" then
+        module_version("StdEnv/2018.3","default")
+else
+        module_version("StdEnv/2016.4","default")
+end
