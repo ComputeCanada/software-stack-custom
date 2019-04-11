bddd65bdd82f6d4e05b382e8ecf516c4347986ee
Bart Oldeman <bart.oldeman@calculquebec.ca>
2019-04-11 15:16:53 +0000

    Revert logic in StdEnv. Use logic in global MODULERCFILE instead.

--- modules/StdEnv/.modulerc.lua
+++ modules/StdEnv/.modulerc.lua
@@ -1,5 +1,1 @@
-if os.getenv("CC_CLUSTER") == "beluga" or (os.getenv("CC_CLUSTER") == "niagara" and os.getenv("RSNT_ARCH") == "avx512") then
-        module_version("StdEnv/2018.3","default")
-else
-        module_version("StdEnv/2016.4","default")
-end
+module_version("StdEnv/2016.4", "default")
