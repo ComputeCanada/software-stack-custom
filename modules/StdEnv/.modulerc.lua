82e9aad7ce7ab367b78422a5e326292885c5452e
Bart Oldeman <bart.oldeman@calculquebec.ca>
2019-04-11 14:07:36 +0000

    Adapt StdEnv to Niagara's setup.

--- modules/StdEnv/.modulerc.lua
+++ modules/StdEnv/.modulerc.lua
@@ -1,4 +1,4 @@
-if os.getenv("CC_CLUSTER") == "beluga" or os.getenv("CC_CLUSTER") == "niagara" then
+if os.getenv("CC_CLUSTER") == "beluga" or (os.getenv("CC_CLUSTER") == "niagara" and os.getenv("RSNT_ARCH") == "avx512") then
         module_version("StdEnv/2018.3","default")
 else
         module_version("StdEnv/2016.4","default")
