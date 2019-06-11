d5f2235a96378144bea3c8c178cb4a35e26a05cd
Bart Oldeman <bart.oldeman@calculquebec.ca>
2019-06-11 18:46:27 +0000

    Revert "Handle MODULERCFILE and default StdEnv versions via StdEnv module."
    This reverts commit 901ca9c211791de24a3af6fbcc3729091057c387.

--- modules/StdEnv/2018.3.lua
+++ modules/StdEnv/2018.3.lua
@@ -3,8 +3,7 @@ add_property(   "lmod", "sticky")
 local root = "/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09"
 
 require("os")
-prepend_path("MODULERCFILE", "/cvmfs/soft.computecanada.ca/config/lmod/modulerc_2018.3")
 load("nixpkgs/16.09")
 load("imkl/2018.3.222")
 load("intel/2018.3")
-load("openmpi")
+load("openmpi/3.1.2")
