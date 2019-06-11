66fcd0b622bdc8118407a37d1f7f2b365e83c400
Bart Oldeman <bart.oldeman@calculquebec.ca>
2019-06-11 18:46:17 +0000

    Revert "Adapt StdEnv to Niagara's setup."
    This reverts commit 82e9aad7ce7ab367b78422a5e326292885c5452e.

--- modules/StdEnv/.modulerc.lua
+++ modules/StdEnv/.modulerc.lua
@@ -1,4 +1,4 @@
-if os.getenv("CC_CLUSTER") == "beluga" or (os.getenv("CC_CLUSTER") == "niagara" and os.getenv("RSNT_ARCH") == "avx512") then
+if os.getenv("CC_CLUSTER") == "beluga" or os.getenv("CC_CLUSTER") == "niagara" then
         module_version("StdEnv/2018.3","default")
 else
         module_version("StdEnv/2016.4","default")
