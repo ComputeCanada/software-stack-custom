bfb244042185d67af6d616326da1cdbb0fabe525
Bart Oldeman <bart.oldeman@calculquebec.ca>
2019-06-11 18:46:07 +0000

    Revert "Revert logic in StdEnv. Use logic in global MODULERCFILE instead."
    This reverts commit bddd65bdd82f6d4e05b382e8ecf516c4347986ee.

--- modules/StdEnv/.modulerc.lua
+++ modules/StdEnv/.modulerc.lua
@@ -1,1 +1,5 @@
-module_version("StdEnv/2016.4", "default")
+if os.getenv("CC_CLUSTER") == "beluga" or (os.getenv("CC_CLUSTER") == "niagara" and os.getenv("RSNT_ARCH") == "avx512") then
+        module_version("StdEnv/2018.3","default")
+else
+        module_version("StdEnv/2016.4","default")
+end
