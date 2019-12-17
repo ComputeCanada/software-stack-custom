32375243a0f44f8b23ce70150825793a4d3013d4
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2019-12-17 19:45:46 +0000

    adding module for singularity 3.5

---
+++ modules/singularity/3.5.lua
@@ -0,0 +1,24 @@
+help([[Singularity is an application containerization solution for High-Performance Computing (HPC). The goal
+of Singularity is to allow for "mobility of computing": an application containerized on one Linux system should 
+be able to run on another system, as it is, and without the need to reconcile software dependencies and Linux 
+version differences between the source and target systems. 
+- Website: http://singularity.lbl.gov/ 
+- CC-Wiki: Singularity ]])
+whatis([[Description: Singularity is an application containerization solution for High-Performance Computing (HPC). The goal
+of Singularity is to allow for "mobility of computing": an application containerized on one Linux system should 
+be able to run on another system, as it is, and without the need to reconcile software dependencies and Linux 
+version differences between the source and target systems. 
+- Website: http://singularity.lbl.gov/ 
+- CC-Wiki: Singularity ]])
+
+local root = "/opt/software/singularity-3.5"
+
+prepend_path("PATH", pathJoin(root, "bin"))
+local slurm_tmpdir = os.getenv("SLURM_TMPDIR") or nil
+local scratch = os.getenv("SCRATCH") or "/tmp"
+if slurm_tmpdir then
+	setenv("SINGULARITY_TMPDIR",slurm_tmpdir)
+else
+	setenv("SINGULARITY_TMPDIR",scratch)
+end
+
