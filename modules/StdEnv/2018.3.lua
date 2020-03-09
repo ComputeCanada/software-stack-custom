28f57d3fc2f8393bca1242ba43d4f9f9df36a20c
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2020-03-09 18:05:02 +0000

    commited a change to the order of loading imkl

--- modules/StdEnv/2018.3.lua
+++ modules/StdEnv/2018.3.lua
@@ -4,7 +4,7 @@ local root = "/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09"
 
 require("os")
 load("nixpkgs/16.09")
-load("imkl/2018.3.222")
 load("CCconfig")
 load("intel/2018.3")
+load("imkl/2018.3.222")
 load("openmpi/3.1.2")
