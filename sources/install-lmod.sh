5836ca01cd80e153ee2ece57e143c4d859fca6e2
Bart Oldeman <bart.oldeman@calculquebec.ca>
2020-11-03 12:15:36 +0000

    Upgrade Lmod to 8.4.12
    Using --with-availExtensions=no to avoid listing extensions
    in "module avail".

--- sources/install-lmod.sh
+++ sources/install-lmod.sh
@@ -4,12 +4,12 @@ cd /tmp/build-lmod
 CUSTOM=/cvmfs/soft.computecanada.ca/custom
 LUA=/cvmfs/soft.computecanada.ca/custom/software/lua
 TCL=/cvmfs/soft.computecanada.ca/custom/software/tcl
-tar xf $CUSTOM/sources/Lmod-8.3.8.tar.gz
-cd Lmod-8.3.8
+tar xf $CUSTOM/sources/Lmod-8.4.12.tar.gz
+cd Lmod-8.4.12
 export CPATH=$TCL/include
 export LIBRARY_PATH=$TCL/lib
 export PATH=$TCL/bin:$LUA/bin:$PATH
-./configure --with-duplicatePaths=yes --with-caseIndependentSorting=yes --with-redirect=yes --with-module-root-path=/cvmfs/soft.computecanada.ca/easybuild/modules --prefix=$CUSTOM/software LIBS="-lm -ldl"
+./configure --with-duplicatePaths=yes --with-caseIndependentSorting=yes --with-redirect=yes --with-module-root-path=/cvmfs/soft.computecanada.ca/easybuild/modules --prefix=$CUSTOM/software --with-availExtensions=no LIBS="-lm -ldl"
 rm -rf $CUSTOM/software/lmod
 make install
 cd ..
