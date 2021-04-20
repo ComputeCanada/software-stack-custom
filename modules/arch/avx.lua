8cfcefd207a0cd5a3b9a71518b197cecabac039b
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2021-04-20 14:09:32 +0000

    defining EBVERSIONARCH to be able to figure out if an arch module is loaded

--- modules/arch/avx.lua
+++ modules/arch/avx.lua
@@ -1,3 +1,4 @@
 if (mode() ~= "spider") then
 	prepend_path("MODULEPATH","/cvmfs/soft.computecanada.ca/custom/modules-avx")
+	setenv("EBVERSIONARCH","avx")
 end
--- modules/arch/avx512.lua
+++ modules/arch/avx512.lua
@@ -1,3 +1,4 @@
 if (mode() ~= "spider") then
 	prepend_path("MODULEPATH","/cvmfs/soft.computecanada.ca/custom/modules-avx512")
+	setenv("EBVERSIONARCH","avx512")
 end
