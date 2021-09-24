7b80291797e54a5b09a972e9c2217221ad712839
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2021-09-24 18:48:42 +0000

    added code to detect the vendor ID of the cpu and load gcc if it is amd

--- modules/StdEnv/2018.3.lua
+++ modules/StdEnv/2018.3.lua
@@ -5,5 +5,9 @@ local root = "/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09"
 require("os")
 load("nixpkgs/16.09")
 load("imkl/2018.3.222")
-load("intel/2018.3")
+if cpu_vendor_id == "amd" then
+	load("gcc/7.3.0")
+else
+	load("intel/2018.3")
+end
 load("openmpi/3.1.2")
