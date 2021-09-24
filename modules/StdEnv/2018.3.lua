de6cfa1bf019a25e26462195f7b7eb48b25b8460
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2021-09-24 19:17:45 +0000

    retrieve RSNT_CPU_VENDOR_ID in 2018.3 module

--- modules/StdEnv/2018.3.lua
+++ modules/StdEnv/2018.3.lua
@@ -5,6 +5,7 @@ local root = "/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09"
 require("os")
 load("nixpkgs/16.09")
 load("imkl/2018.3.222")
+local cpu_vendor_id = os.getenv("RSNT_CPU_VENDOR_ID")
 if cpu_vendor_id == "amd" then
 	load("gcc/7.3.0")
 else
