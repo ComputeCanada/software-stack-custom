6858ecfa68cedcc51e4805074f505e01ba6e6637
Bart Oldeman <bart.oldeman@calculquebec.ca>
2022-04-06 14:37:24 +0000

    Add module for apptainer/1.0

---
+++ modules/apptainer/1.0.lua
@@ -0,0 +1,24 @@
+help([[Apptainer/Singularity is an application containerization solution for High-Performance Computing (HPC). The goal
+of Apptainer is to allow for "mobility of computing": an application containerized on one Linux system should 
+be able to run on another system, as it is, and without the need to reconcile software dependencies and Linux 
+version differences between the source and target systems. 
+- Website: https://apptainer.org
+- CC-Wiki: Apptainer ]])
+whatis([[Description: Apptainer/Singularity is an application containerization solution for High-Performance Computing (HPC). The goal
+of Apptainer is to allow for "mobility of computing": an application containerized on one Linux system should 
+be able to run on another system, as it is, and without the need to reconcile software dependencies and Linux 
+version differences between the source and target systems. 
+- Website: https://apptainer.org
+- CC-Wiki: Apptainer ]])
+
+local root = "/opt/software/apptainer-1.0"
+
+prepend_path("PATH", pathJoin(root, "bin"))
+local slurm_tmpdir = os.getenv("SLURM_TMPDIR") or nil
+local scratch = os.getenv("SCRATCH") or "/tmp"
+if slurm_tmpdir then
+	setenv("APPTAINER_TMPDIR",slurm_tmpdir)
+else
+	setenv("APPTAINER_TMPDIR",scratch)
+end
+
