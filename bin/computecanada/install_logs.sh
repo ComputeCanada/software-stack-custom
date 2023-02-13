658cfc9d9b96ea19bbbfda9cda9e4c82cac084ef
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2023-02-13 17:15:18 +0000

    show details about each version (and also accept versionned-arguments

--- bin/computecanada/install_logs.sh
+++ bin/computecanada/install_logs.sh
@@ -6,8 +6,16 @@ if [[ -z $EBROOTGENTOO ]]; then
 else
 	YEAR="2020"
 fi
+
 names=$(for r in $(find . -iname "$1*.eb"); do echo $(basename $(dirname $r)); done | sort | uniq)
 for name in $names; do
 	echo "To see updates and who installed $name; visit https://github.com/ComputeCanada/easybuild-easyconfigs-installed-$RSNT_ARCH/commits/main/$YEAR/$name"
 done
 
+echo "==========================================================="
+for version in $(find . -iname "$1*.eb"); do 
+	filename=$(basename $version)
+	name=$(basename $(dirname $version))
+	echo "To see updates and who installed $filename; visit https://github.com/ComputeCanada/easybuild-easyconfigs-installed-$RSNT_ARCH/commits/main/$YEAR/$name/$filename"
+done
+echo "==========================================================="
