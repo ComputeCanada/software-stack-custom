a7753c9da84f0c9ce24aee1a7f4cdb80ebf21448
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-07-10 20:43:37 +0000

    load ipython-kernel by default as it used to be the case on our clusters' jupyterlab

--- modules/jupyterlab-apps/1.0.lua
+++ modules/jupyterlab-apps/1.0.lua
@@ -2,4 +2,5 @@ help([[jupyterlab-apps is a collection of modules that make applications availab
 depends_on("code-server/4.101")
 depends_on("libreqda/1")
 depends_on("rstudio-server")
+depends_on("ipython-kernel")
 
