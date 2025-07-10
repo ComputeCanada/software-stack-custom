ff6b6d8714160a4db7048cd6b69fc5bcd9d4e698
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-07-10 13:16:40 +0000

    add module jupyterlab-apps

---
+++ modules/jupyterlab-apps/1.0.lua
@@ -0,0 +1,5 @@
+help([[jupyterlab-apps is a collection of modules that make applications available in JupyterLab]])
+depends_on("code-server/4.101")
+depends_on("libreqda/1")
+depends_on("rstudio-server")
+
