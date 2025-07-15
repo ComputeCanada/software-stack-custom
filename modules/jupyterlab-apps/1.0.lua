0ffbc20322802c15ef794afd8ed287751cea7998
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-07-15 13:38:31 +0000

    set JUPYTER_APP_LAUNCHER_TEMPLATES_PATH to a different path if the path for the cluster is not set

--- modules/jupyterlab-apps/1.0.lua
+++ modules/jupyterlab-apps/1.0.lua
@@ -4,11 +4,17 @@ depends_on("libreqda/1")
 depends_on("rstudio-server")
 depends_on("ipython-kernel")
 
+local posix = require "posix"
+
 local cc_cluster = os.getenv("CC_CLUSTER")
 local slurm_tmpdir = os.getenv("SLURM_TMPDIR") or nil
 local slurm_job_id = os.getenv("SLURM_JOB_ID") or nil
 if slurm_tmpdir and slurm_job_id then
 	setenv('JUPYTER_APP_LAUNCHER_PATH', slurm_tmpdir)
-	setenv('JUPYTER_APP_LAUNCHER_TEMPLATES_PATH', '/cvmfs/soft.computecanada.ca/config/jupyterhub_node/v6/share/jupyter/jupyter_app_launcher/' .. cc_cluster)
+	if posix.stat(pathJoin("/cvmfs/soft.computecanada.ca/config/jupyterhub_node/v6/share/jupyter/jupyter_app_launcher/", cc_cluster),"type") == 'directory' then
+		setenv('JUPYTER_APP_LAUNCHER_TEMPLATES_PATH', '/cvmfs/soft.computecanada.ca/config/jupyterhub_node/v6/share/jupyter/jupyter_app_launcher/' .. cc_cluster)
+	else
+		setenv('JUPYTER_APP_LAUNCHER_TEMPLATES_PATH', '/cvmfs/soft.computecanada.ca/config/jupyterhub_node/v6/share/jupyter/jupyter_app_launcher/')
+	end
 	execute {cmd="/cvmfs/soft.computecanada.ca/config/jupyterhub_node/scripts/jp_app_launcher_templater.py", modeA={"load"}}
 end
