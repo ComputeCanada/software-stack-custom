ab32edbc325e3b682f2f4310311d35da6278ac7b
Bart Oldeman <bart.oldeman@calculquebec.ca>
2025-10-21 13:29:24 +0000

    Upgrade Lmod to 9.0.2
    I ran into https://github.com/TACC/Lmod/issues/780 with 8.7.65
    so trying a newer one.
    
    Also the French typo fix was merged there.

--- sources/install-lmod.sh
+++ sources/install-lmod.sh
@@ -4,11 +4,12 @@ cd /tmp/build-lmod
 CUSTOM=/cvmfs/soft.computecanada.ca/custom
 LUA=/cvmfs/soft.computecanada.ca/custom/software/lua
 TCL=/cvmfs/soft.computecanada.ca/custom/software/tcl
-tar xf $CUSTOM/sources/Lmod-8.7.47.tar.gz
-cd Lmod-8.7.47
+tar xf $CUSTOM/sources/Lmod-9.0.2.tar.gz
+cd Lmod-9.0.2
 export CPATH=$TCL/include
 export LIBRARY_PATH=$TCL/lib
 export PATH=$TCL/bin:$LUA/bin:$PATH
+sed -i "s@TCL_PATH=.*@TCL_PATH=$TCL/bin/tclsh8.6/@" proj_mgmt/find_tcl_pc.sh
 ./configure --with-duplicatePaths=yes --with-caseIndependentSorting=yes --with-redirect=yes --with-module-root-path=/cvmfs/soft.computecanada.ca/easybuild/modules --prefix=$CUSTOM/software --with-availExtensions=no LIBS="-lm -ldl"
 rm -rf $CUSTOM/software/lmod
 make install
