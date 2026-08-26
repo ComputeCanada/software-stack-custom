9040d05a202a96452d9fa5fd3a5c23dde3c36887
Bart Oldeman <bart.oldeman@calculquebec.ca>
2026-08-26 14:41:55 +0000

    Add StdEnv/2026 module

---
+++ modules/StdEnv/2026.lua
@@ -0,0 +1,39 @@
+add_property(   "lmod", "sticky")
+
+require("os")
+load("CCconfig")
+load("gentoo/2026")
+
+-- set by gentoo/2026 module
+local arch = os.getenv("RSNT_ARCH")
+if arch == "avx2" then
+	newarch = "x86-64-v3"
+elseif arch == "avx512" then
+	newarch = "x86-64-v4"
+end
+-- gentoo/2026 will not load for sse3 and avx
+
+if (mode() == "spider" and (arch == "avx2" or arch == "avx512")) then
+	local coresubdir = "easybuild/modules/2026/x86-64-v3"
+	local subdir = pathJoin("easybuild/modules/2026", newarch)
+	prepend_path("MODULEPATH", pathJoin("/cvmfs/soft.computecanada.ca", coresubdir, "Core"))
+	prepend_path("MODULEPATH", pathJoin("/cvmfs/soft.computecanada.ca", subdir, "Compiler/gcccore"))
+	local user = os.getenv("USER","unknown")
+	local home = os.getenv("HOME",pathJoin("/home",user))
+	if user ~= "ebuser" then
+		prepend_path("MODULEPATH", pathJoin(home, ".local", coresubdir, "Core"))
+		prepend_path("MODULEPATH", pathJoin(home, ".local", subdir, "Compiler/gcccore"))
+	end
+end
+
+load("gcc")
+load("openmpi")
+load("flexiblas")
+local cpu_vendor_id = os.getenv("RSNT_CPU_VENDOR_ID")
+if cpu_vendor_id == "amd" then
+	load("aocl-blas")
+	load("aocl-lapack")
+	setenv("FLEXIBLAS", "aocl")
+else
+	load("imkl")
+end
